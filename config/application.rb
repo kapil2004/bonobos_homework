2a14eb7a4a08a7116b30432cd373c53bd98cd8bc
Kapil Gupta <kapil2004@gmail.com>
2018-05-18 14:33:47 -0400

    Add CORS config

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Bonobos
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'http://localhost:8080',
+      'Access-Control-Request-Method' => %w{GET OPTIONS}.join(",")
+    }
   end
 end
